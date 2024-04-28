2a9990910e48ee1012ca75d87bd3bfdb2a5d4eeb
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2024-04-28 09:13:12 +0600

    get and post from database successfull

--- index.js
+++ index.js
@@ -11,11 +11,56 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.post('/add',(req, res) => {
-  const formData = req.body;
-  console.log(formData);
-})
+// app.post('/add', (req, res) => {
+//   const formData = req.body;
+//   console.log(formData);
+// })
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://esha-craft:lOeiB4BYxxDZQcBv@cluster0.jcb8og7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const collection = client.db("esha-craft").collection("craftItem");
+
+    app.get('/all', async (req, res) => {
+      const cursor = collection.find();
+      const result = await cursor.toArray();
+      
+      res.send(result)
+    })
+
+    app.post('/add', async (req, res) => {
+      const formData = req.body;
+      console.log(formData);
+      const result = await collection.insertOne(formData);
+      res.send(result)
+    });
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.listen(port, () => {
