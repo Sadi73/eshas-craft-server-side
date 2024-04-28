9d65ea22cab6b9f5d2752305ce38df499197238a
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2024-04-28 08:02:50 +0600

    post api added

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+});
+
+app.post('/add',(req, res) => {
+  const formData = req.body;
+  console.log(formData);
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
